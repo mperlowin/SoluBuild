aab7c41abbffe002f2b70a12c4709a2dc55f75a2
Michael Perlowin <82491078+mperlowin@users.noreply.github.com>
2024-08-21 21:47:28 +0000

    update

--- flaskApp.py
+++ flaskApp.py
@@ -1,10 +1,14 @@
 import os
-from flask import Flask, request, jsonify, Response
-from twilio.twiml.voice_response import VoiceResponse
+from flask import Flask, request, jsonify, Response, render_template
+from flask_sockets import Sockets
+from twilio.twiml.voice_response import Connect, VoiceResponse, Say, Start, Stream
 import ngrok
 import asyncio
 import nest_asyncio
 import signal
+import logging
+import json
+import base64
 try:
     from google.colab import userdata
     IN_COLAB = True
@@ -13,10 +17,13 @@ except ImportError:
     from dotenv import load_dotenv
     load_dotenv()
 
+app = Flask(__name__)
+sockets = Sockets(app)
+
 class FlaskApp:
     def __init__(self):
         # Initialize Flask app
-        self.app = Flask(__name__)
+        self.app = app
         self.port = 5000
         self.setup_routes()
         nest_asyncio.apply()
@@ -28,18 +35,56 @@ class FlaskApp:
         def home():
             return "Hello, World!"
         
-        @self.app.route('/twiml', methods=['GET', 'POST'])
-        def twiml():
+        @self.app.route('/test', methods=['POST'])
+        def test():
+            data = request.data
+            print(f"Data received: {data}")
+            return jsonify({"message": "Data received successfully"}), 200
+        
+        @self.app.route('/twiml', methods=['POST'])
+        def return_twiml():
             response = VoiceResponse()
-            response.stream(url=self.listener.url().replace("https", "wss") + "/audio")
+            connect = Connect()
+            connect.stream(url=f'{self.listener.url().replace('https', 'wss')}/audio')
+            response.append(connect)
+            response.say(
+                'cash cash money hoe'
+            )
+            print(response)
             return Response(str(response), mimetype='text/xml')
 
-        @self.app.route('/audio', methods=['POST'])
-        def process_audio():
-            data = request.data
-            with open('audio_stream.raw', 'wb') as f:
-                f.write(data)
-            return jsonify({"message": "Data received successfully"}), 200
+        @sockets.route('/audio')
+        def echo(ws):
+            app.logger.info("Connection accepted")
+            # A lot of messages will be sent rapidly. We'll stop showing after the first one.
+            has_seen_media = False
+            message_count = 0
+            while not ws.closed:
+                message = ws.receive()
+                if message is None:
+                    app.logger.info("No message received...")
+                    continue
+                        # Messages are a JSON encoded string
+                data = json.loads(message)
+
+                # Using the event type you can determine what type of message you are receiving
+                if data['event'] == "connected":
+                    app.logger.info("Connected Message received: {}".format(message))
+                if data['event'] == "start":
+                    app.logger.info("Start Message received: {}".format(message))
+                if data['event'] == "media":
+                    if not has_seen_media:
+                        app.logger.info("Media message: {}".format(message))
+                        payload = data['media']['payload']
+                        app.logger.info("Payload is: {}".format(payload))
+                        chunk = base64.b64decode(payload)
+                        app.logger.info("That's {} bytes".format(len(chunk)))
+                        app.logger.info("Additional media messages from WebSocket are being suppressed....")
+                        has_seen_media = True
+                if data['event'] == "closed":
+                    app.logger.info("Closed Message received: {}".format(message))
+                    break
+                message_count += 1
         
         @self.app.route('/shutdown', methods=['POST', 'GET'])
         async def shutdown():
@@ -49,15 +94,19 @@ class FlaskApp:
             return 'Server shutting down...'
     
     async def setup_ngrok(self):
-        token = userdata.get("") if IN_COLAB else os.getenv("")
+        token = userdata.get("NGROK_AUTHTOKEN") if IN_COLAB else os.getenv("NGROK_AUTHTOKEN")
         ngrok.set_auth_token(token)
         self.listener = await ngrok.forward(f"localhost:{self.port}")
         print(f"Ingress established at {self.listener.url()}")
             
 
     def start_flask_app(self):
-        self.app.run(port=self.port)
-        print(f"Flask app running on port: {self.port}.")
+        self.app.logger.setLevel(logging.DEBUG)
+        from gevent import pywsgi
+        from geventwebsocket.handler import WebSocketHandler
+        server = pywsgi.WSGIServer(('', self.port), app, handler_class=WebSocketHandler)
+        print("Server listening on: http://localhost:" + str(self.port))
+        server.serve_forever()
 
 def shutdown_server():
     pid = os.getpid()
@@ -65,15 +114,4 @@ def shutdown_server():
 
 import threading
 
-# Global variable to store the FlaskApp instance lol
-flask_app_instance = None
-
-def run_flask_app():
-    global flask_app_instance
-    flask_app_instance = FlaskApp()
-
-# Create a thread targeting the run_flask_app function
-flask_thread = threading.Thread(target=run_flask_app)
-
-# Start the thread
-flask_thread.start()
+FlaskApp()
